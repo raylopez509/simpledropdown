5fb2702286092ed84a90bef9b3be8ab47d0232a4
Ray Lopez <raylopez509@gmail.com>
2025-02-18 10:17:27 -0800

    added source code

---
+++ script.js
@@ -0,0 +1,28 @@
+function dropdown (dropdownClass,dropdownItemClass) {
+  const expandButton = document.querySelector(`.${dropdownClass}`);
+  expandButton.addEventListener("mouseover", showDropdownItems);
+  expandButton.addEventListener ("mouseout", hideDropdownItems);
+
+  const dropdownItems = document.querySelectorAll(`.${dropdownItemClass}`);
+  for(const item of dropdownItems) {
+    item.addEventListener("mouseover", showDropdownItems);
+    item.addEventListener ("mouseout", hideDropdownItems);
+    item.style.display = "none";
+  }
+
+  function showDropdownItems() {
+    for(const item of dropdownItems) {
+      item.style.display = "block";
+    }    
+  }
+
+  function hideDropdownItems() {
+    for(const item of dropdownItems) {
+      item.style.display = "none";
+    } 
+  } 
+
+dropdown('dropdown','dropdown-item');
+dropdown('test','item');
+
+
